acbf20b2c94fc2fc47e3a6c320938fc8c340feb0
Chabane <chabane.refes@gmail.com>
2017-02-26 22:04:31 +0100

    write nodejs pipeline

--- jenkins/dsl/nodejs_pipelineJob.groovy
+++ jenkins/dsl/nodejs_pipelineJob.groovy
@@ -4,12 +4,27 @@ pipelineJob("nodejs-pipeline") {
             sandbox()
             script("""
                 node {
-                      stage "checkout"
+                      tools {
+                       nodejs 'Node 6.x'
+                      }
 
+                      stage "checkout"
+                       git url : 'https://github.com/NirbyApp/mitosis-microservice-nodejs-angular.git'
+ 
+                      stage "install"
+                       sh 'npm install -g yarn'
+                       sh 'yarn'
+                          
                       stage "test"
+                       sh 'npm run test -watch=false'
+
+                      stage "build"
+                       sh 'npm run build'
 
                       stage "deploy"
-                }                 
+                       sh 'docker build -t mitosis/microservice-nodejs .'
+                       sh 'docker service create --name microservice-nodejs --publish 9992:80 --network microservices-net --replicas 2 mitosis/microservice-nodejs'
+                }             
             """.stripIndent())
         }
     }
