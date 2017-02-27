3c381f02e79470b278c94079dd2c5afe72bdf2b6
Havrileck Alexandre <oxyno-zeta@users.noreply.github.com>
2017-02-27 13:11:06 +0100

    Better stage and use of default yarn and nvm

--- jenkins/dsl/nodejs_pipelineJob.groovy
+++ jenkins/dsl/nodejs_pipelineJob.groovy
@@ -4,26 +4,28 @@ pipelineJob("nodejs-pipeline") {
             sandbox()
             script("""
                 node {
-                      tools {
-                       nodejs 'Node 6.x'
-                      }
+                      sh 'nvm use 6'
 
-                      stage "checkout"
-                       git url : 'https://github.com/NirbyApp/mitosis-microservice-nodejs-angular.git'
+                        stage ("checkout") {
+                            git url : 'https://github.com/NirbyApp/mitosis-microservice-nodejs-angular.git'
+                        }
  
-                      stage "install"
-                       sh 'npm install -g yarn'
-                       sh 'yarn'
+                        stage ("install") {
+                            sh 'yarn'
+                        }
                           
-                      stage "test"
-                       sh 'npm run test -watch=false'
-
-                      stage "build"
-                       sh 'npm run build'
-
-                      stage "deploy"
-                       sh 'docker build -t mitosis/microservice-nodejs .'
-                       sh 'docker service create --name microservice-nodejs --publish 9992:80 --network microservices-net --replicas 2 mitosis/microservice-nodejs'
+                        stage ("test") {
+                            sh 'npm run test -watch=false'
+                        }
+                       
+                        stage ("build") {
+                            sh 'npm run build'
+                        }
+                       
+                        stage ("deploy"){
+                            sh 'docker build -t mitosis/microservice-nodejs .'
+                            sh 'docker service create --name microservice-nodejs --publish 9992:80 --network microservices-net --replicas 2 mitosis/microservice-nodejs'
+                        }                      
                 }             
             """.stripIndent())
         }
