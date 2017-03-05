b54f1d4137b11788628b68019db836288a19f1a6
Chabane <chabane.refes@gmail.com>
2017-03-06 00:02:32 +0100

    add condition to create/update a service

--- jenkins/dsl/nodejs_pipelineJob.groovy
+++ jenkins/dsl/nodejs_pipelineJob.groovy
@@ -4,29 +4,35 @@ pipelineJob("nodejs-pipeline") {
             sandbox()
             script("""
                 node {
-                   docker.image('node:boron').inside {
+                    docker.image('node:boron').inside {
 
-                        stage ("checkout") {
-                            git url : 'https://github.com/NirbyApp/mitosis-microservice-nodejs-angular.git'
-                        }
- 
-                        stage ("install") {
-                            sh 'yarn'
-                        }
-                          
-                        stage ("test") {
-                        }
-                       
-                        stage ("build") {
-                            sh 'npm run build:prod'
-                        }
-                       
-                        stage ("deploy"){
-                            sh 'docker build -t mitosis/microservice-nodejs .'
-                            sh 'docker service create --name microservice-nodejs --publish 9992:80 --network microservices-net --replicas 2 mitosis/microservice-nodejs'
-                        }       
-                    }               
-                }             
+                            def retstat = sh(script: 'docker service inspect microservice-nodejs', returnStatus: true)
+
+                            stage ("checkout") {
+                                git url : 'https://github.com/NirbyApp/mitosis-microservice-nodejs-angular.git'
+                            }
+
+                            stage ("install") {
+                                sh 'yarn'
+                            }
+
+                            stage ("test") {
+                            }
+
+                            stage ("build") {
+                                sh 'npm run build:prod'
+                            }
+
+                            stage ("deploy"){
+                                sh 'docker build -t mitosis/microservice-nodejs .'
+                                if (retstat == 1) {
+                                    sh 'docker service create --name microservice-nodejs --publish 9992:80 --network microservices-net --replicas 2 mitosis/microservice-nodejs'
+                                } else {
+                                    sh 'docker service update --replicas 2 --image mitosis/microservice-nodejs microservice-nodejs'
+                                }    
+                            }       
+                        }                             
+                    }             
             """.stripIndent())
         }
     }
--- jenkins/dsl/spring5_gradle_pipelineJob.groovy
+++ jenkins/dsl/spring5_gradle_pipelineJob.groovy
@@ -4,6 +4,8 @@ pipelineJob("spring5-gradle-pipeline") {
             sandbox()
             script("""
                 node {
+                      def retstat = sh(script: 'docker service inspect microservice-spring-reactor', returnStatus: true)
+
                       stage "checkout"
                        git url : 'https://github.com/NirbyApp/mitosis-microservice-spring-reactor.git'
                            
@@ -15,7 +17,11 @@ pipelineJob("spring5-gradle-pipeline") {
 
                       stage "deploy"
                        sh 'docker build -t mitosis/microservice-spring-reactor .'
-                       sh 'docker service create --name microservice-spring-reactor --publish 9991:8080 --network microservices-net --replicas 2 mitosis/microservice-spring-reactor'
+                       if (retstat == 1) {
+                          sh 'docker service create --name microservice-spring-reactor --publish 9991:8080 --network microservices-net --replicas 2 mitosis/microservice-spring-reactor'  
+                       } else {
+                           sh 'docker service update --replicas 2 --image mitosis/microservice-spring-reactor microservice-spring-reactor'
+                       }            
                 }                 
             """.stripIndent())
         }
