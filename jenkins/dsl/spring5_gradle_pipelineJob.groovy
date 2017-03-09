b0094e324eb09115afaa8b0944a033ab14feb283
Chabane <chabane.refes@gmail.com>
2017-03-09 10:33:51 +0100

    externalise jenkins file

--- jenkins/dsl/spring5_gradle_pipelineJob.groovy
+++ jenkins/dsl/spring5_gradle_pipelineJob.groovy
@@ -1,32 +1,17 @@
 pipelineJob('spring5-gradle-pipeline') {
+    scm {
+        git {
+            remote {
+                github("NirbyApp/mitosis-microservice-spring-reactor")
+            }
+        }
+    }
     definition {
-        cps {
-            sandbox()
-            script("""
-                node {
-                      def retstat = sh(script: 'docker service inspect microservice-spring-reactor', returnStatus: true)
-
-                      stage ('checkout') {
-                        git url : 'https://github.com/NirbyApp/mitosis-microservice-spring-reactor.git'
-                      }     
-                      stage ('test') {
-                        sh './gradlew test'
-                      }
-
-                      stage ('build') {
-                        sh './gradlew build'
-                      }
-
-                      stage ('deploy') {
-                        sh 'docker build -t mitosis/microservice-spring-reactor .'
-                        if (retstat == 1) {
-                            sh 'docker service create --name microservice-spring-reactor --publish 9991:8080 --network microservices-net --replicas 2 mitosis/microservice-spring-reactor'  
-                        } else {
-                            sh 'docker service update --replicas 2 --image mitosis/microservice-spring-reactor microservice-spring-reactor'
-                        }            
-                     }
-                }                 
-            """.stripIndent())
+        cpsScm {
+            scm {
+                github("NirbyApp/mitosis-microservice-spring-reactor", "master")
+            }
+            scriptPath("Jenkinsfile")
         }
     }
 }
