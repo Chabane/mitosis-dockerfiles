1d8137271c5ef795c12aba8ac36c0e74c08e3b90
Chabane <chabane.refes@gmail.com>
2017-09-22 19:37:37 +0200

    cassandra: fix

--- cassandra/entrypoint-wrap.sh
+++ cassandra/entrypoint-wrap.sh
@@ -3,7 +3,7 @@
 if [[ ! -z "$CASSANDRA_KEYSPACE" && $1 = 'cassandra' ]]; then
   # Create default keyspace for single node cluster
   CQL="CREATE KEYSPACE $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-  CQL+="CREATE TABLE mitosis.cell (name varchar, cellType varchar, PRIMARY KEY (name, cellType));"
+  CQL+="CREATE TABLE $CASSANDRA_KEYSPACE.cell (name varchar, cellType varchar, PRIMARY KEY (name, cellType));"
   until echo $CQL | cqlsh; do
     echo "cqlsh: Cassandra is unavailable - retry later"
     sleep 2
