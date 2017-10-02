6dc138d9f18cdba45ffe3a39e9d534a9e16a54c3
Chabane <chabane.refes@gmail.com>
2017-10-02 18:43:54 +0200

    bugfix: fix query format

--- cassandra/entrypoint-wrap.sh
+++ cassandra/entrypoint-wrap.sh
@@ -2,8 +2,8 @@
 
 if [[ ! -z "$CASSANDRA_KEYSPACE" && $1 = 'cassandra' ]]; then
   # Create default keyspace for single node cluster
-  CQL="CREATE KEYSPACE $CASSANDRA_KEYSPACE IF NOT EXISTS WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-  CQL+="CREATE TABLE IF NOT EXISTS $CASSANDRA_KEYSPACE.cell (name varchar, cellType varchar, PRIMARY KEY (name, cellType));"
+  CQL="CREATE KEYSPACE IF NOT EXISTS $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+  CQL+="CREATE TABLE IF NOT EXISTS $CASSANDRA_KEYSPACE.cell (name varchar PRIMARY KEY, celltype varchar);"
   until echo $CQL | cqlsh; do
     echo "cqlsh: Cassandra is unavailable - retry later"
     sleep 2
