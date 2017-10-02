d048fa037e3e8d1d220a8f0305a7c4d39bc3479b
Chabane <chabane.refes@gmail.com>
2017-10-02 22:12:18 +0200

    feature: add sbt installation

--- jenkins/init/3-global-tool-configuration.groovy
+++ jenkins/init/3-global-tool-configuration.groovy
@@ -11,4 +11,19 @@ def gradle =  new hudson.plugins.gradle.GradleInstallation("GRADLE_TOOL", env.GR
 desc.setInstallations(gradle)
 desc.save()
 
+plugin=Jenkins.instance.getExtensionList(org.jvnet.hudson.plugins.SbtPluginBuilder.DescriptorImpl.class)[0];
+tool = plugin.installations.find {
+  it.name == "sbt"
+}
+
+if (tool == null) {
+  println "Registering sbt tool"
+  i=(plugin.installations as List);
+  i.add(new org.jvnet.hudson.plugins.SbtPluginBuilder.SbtInstallation("sbt", "/usr/share/sbt/bin/sbt-launch.jar", "", []));
+  plugin.installations=i
+  plugin.save()
+} else {
+  println "sbt tool has been already registered"
+}
+
 jenkins.save()
